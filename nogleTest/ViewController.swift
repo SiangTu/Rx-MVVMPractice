a076b689f5ecea591faf5680be65c4ea84e76ed1
siang <siangtu.d@gmail.com>
2023-07-22 17:22:09 +0800

    Initial Commit

---
+++ nogleTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  nogleTest
+//
+//  Created by 杜襄 on 2023/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
