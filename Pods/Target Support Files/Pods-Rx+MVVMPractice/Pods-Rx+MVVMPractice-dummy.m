46370915b7adcda392a074fa3a27c6a11bfa5c00
Sean <Sean_Tu@cmoney.com.tw>
2023-07-26 15:17:44 +0800

    feat: rename project

---
+++ Pods/Target Support Files/Pods-Rx+MVVMPractice/Pods-Rx+MVVMPractice-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Rx_MVVMPractice : NSObject
+@end
+@implementation PodsDummy_Pods_Rx_MVVMPractice
+@end
--- Pods/Target Support Files/Pods-Rx+MVVMPractice/Pods-Rx+MVVMPractice-umbrella.h
+++ Pods/Target Support Files/Pods-Rx+MVVMPractice/Pods-Rx+MVVMPractice-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_nogleTestVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_nogleTestVersionString[];
+FOUNDATION_EXPORT double Pods_Rx_MVVMPracticeVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Rx_MVVMPracticeVersionString[];
 
